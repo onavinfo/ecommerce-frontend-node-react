51c29f1f6215b155c2887c9d3f05dfb96bcf32a8
onavinfo <onavinfosolutions@gmail.com>
2026-06-20 13:19:40 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // client/src/socket.js
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "https://ecommerce-backend-node-demo.netlify.app";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,           // you connect manually in component
